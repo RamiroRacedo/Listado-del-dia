0ec09d9798a8f8795e06d158abb2d2752a4d1063
Celeste <celestetorres@epet12smandes.edu.ar>
2022-06-13 11:53:33 -0300

    añadido al repositorio

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path')
+
+app.disableHardwareAcceleration()
 
 function createWindow() {
     const win = new BrowserWindow ({
@@ -9,18 +12,53 @@ function createWindow() {
         frame:false,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            devTools: true
         }
-    })
+  })
 
-    win.loadFile("index.html")
+win.loadFile(`${__dirname}/index.html`);
 
 
-    ipcMain.on('minimizeApp', () => {
-        win.minimize()
-    })
-}
-
-app.whenReady(). then() => {
-    createWindow()
+ipcMain.on('minimizeApp', () => {
+  win.minimize()
 })
+
+ipcMain.on('maximizeRestoreApp', () => {
+  if (win.isMaximized()) win.unmaximize()
+  else win.maximize()
+})
+
+ipcMain.on('closeApp', () => {
+  win.close()
+})
+
+
+win.on("maximize", () => {
+  win.webContents.send("isMaximized")
+})
+win.on("unmaximize", () => {
+  win.webContents.send("isRestored")
+})
+
+
+win.on("focus", () => {
+  win.webContents.send("isFocus")
+})
+win.on("blur", () => {
+  win.webContents.send("isBlur")
+})
+
+app.whenReady().then(() => {
+createWindow()
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+})
+
+app.on('window-all-closed', function () {
+if (process.platform !== 'darwin') app.quit()
+})
+
+}
