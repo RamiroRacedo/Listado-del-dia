da759a34e4d43ff166b06b135cc064dd816404e9
Celeste <celestetorres@epet12smandes.edu.ar>
2022-06-24 10:51:48 -0300

    Añadido los botones y imagenes

--- index.js
+++ index.js
@@ -12,61 +12,61 @@ var nav = null;
 fetch('components/nav.html')
   .then(res => res.text())
   .then(htmlNav => {
-    
+
     let oldElement = document.querySelector("script#replace_with_navbar");
     let newElement = document.createElement("div");
     newElement.innerHTML = htmlNav;
     oldElement.parentNode.replaceChild(newElement, oldElement);
 
-const closeBtn = document.getElementById("closeBtn")
-const minimizeBtn = document.getElementById("minimizeBtn")
-const maimizeBtn = document.getElementById("maximizeBtn")
-const showHideMenus = document.getElementById("showHideMenus");
-const nav = document.getElementById("nav");
+    const closeBtn = document.getElementById("closeBtn")
+    const minimizeBtn = document.getElementById("minimizeBtn")
+    const maimizeBtn = document.getElementById("maximizeBtn")
+    const showHideMenus = document.getElementById("showHideMenus");
+    const nav = document.getElementById("nav");
 
-minimizeBtn.addEventListener('click', () => {
-    ipcRenderer.send('minimizeApp')
+    minimizeBtn.addEventListener('click', () => {
+      ipcRenderer.send('minimizeApp')
+    })
+    maxResBtn.addEventListener('click', () => {
+      ipcRenderer.send('maximizeRestoreApp')
+    })
+    closeBtn.addEventListener('click', () => {
+      ipcRenderer.send('closeApp')
+    })
+    showHideMenus.addEventListener('click', () => {
+      if (isLeftMenuAactive) {
+        mySidebar.style.width = '0px'
+        isLeftMenuAactive = false
+      } else {
+        mySidebar.style.width = '280px'
+        isLeftMenuAactive = true
+      }
+    })
   })
-  maxResBtn.addEventListener('click', () => {
-    ipcRenderer.send('maximizeRestoreApp')
-  })
-  closeBtn.addEventListener('click', () => {
-    ipcRenderer.send('closeApp')
-  })
-  showHideMenus.addEventListener('click', () => {
-    if (isLeftMenuAactive) {
-      mySidebar.style.width = '0px'
-      isLeftMenuAactive = false
-    } else {
-      mySidebar.style.width = '280px'
-      isLeftMenuAactive = true
-    }
-  })
-})
 
 
 function changeMaxResBtn(isMaximizedApp) {
-if (isMaximizedApp) {
-  maxResBtn.title = 'Restore'
-  maxResBtn.classList.remove("maximizeBtn")
-  maxResBtn.classList.add("restoreBtn")
-} else {
-  maxResBtn.title = 'Maximize'
-  maxResBtn.classList.remove("restoreBtn")
-  maxResBtn.classList.add("maximizeBtn")
-}
+  if (isMaximizedApp) {
+    maxResBtn.title = 'Restore'
+    maxResBtn.classList.remove("maximizeBtn")
+    maxResBtn.classList.add("restoreBtn")
+  } else {
+    maxResBtn.title = 'Maximize'
+    maxResBtn.classList.remove("restoreBtn")
+    maxResBtn.classList.add("maximizeBtn")
+  }
 }
 
 ipcRenderer.on("isMaximized", () => {
-changeMaxResBtn(true)
+  changeMaxResBtn(true)
 })
 ipcRenderer.on("isRestored", () => {
-changeMaxResBtn(false)
+  changeMaxResBtn(false)
 })
 
 ipcRenderer.on("isFocus", () => {
-nav.classList.remove("blur")
+  nav.classList.remove("blur")
 })
 ipcRenderer.on("isBlur", () => {
-nav.classList.add("blur")
+  nav.classList.add("blur")
 })
--- main.js
+++ main.js
@@ -4,61 +4,62 @@ const path = require('path')
 app.disableHardwareAcceleration()
 
 function createWindow() {
-    const win = new BrowserWindow ({
-        width:1200,
-        height: 600,
-        minWidth: 600,
-        minHeight: 480,
-        frame:false,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            devTools: true
-        }
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    minWidth: 600,
+    minHeight: 480,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      devTools: true
+    }
   })
 
-win.loadFile(`${__dirname}/index.html`);
+  win.loadFile(`${__dirname}/index.html`);
 
 
-ipcMain.on('minimizeApp', () => {
-  win.minimize()
-})
+  ipcMain.on('minimizeApp', () => {
+    win.minimize()
+  })
 
-ipcMain.on('maximizeRestoreApp', () => {
-  if (win.isMaximized()) win.unmaximize()
-  else win.maximize()
-})
+  ipcMain.on('maximizeRestoreApp', () => {
+    if (win.isMaximized()) win.unmaximize()
+    else win.maximize()
+  })
 
-ipcMain.on('closeApp', () => {
-  win.close()
-})
-
-
-win.on("maximize", () => {
-  win.webContents.send("isMaximized")
-})
-win.on("unmaximize", () => {
-  win.webContents.send("isRestored")
-})
+  ipcMain.on('closeApp', () => {
+    win.close()
+  })
 
 
-win.on("focus", () => {
-  win.webContents.send("isFocus")
-})
-win.on("blur", () => {
-  win.webContents.send("isBlur")
-})
+  win.on("maximize", () => {
+    win.webContents.send("isMaximized")
+  })
+  win.on("unmaximize", () => {
+    win.webContents.send("isRestored")
+  })
+
+
+  win.on("focus", () => {
+    win.webContents.send("isFocus")
+  })
+  win.on("blur", () => {
+    win.webContents.send("isBlur")
+  })
+
+}
 
 app.whenReady().then(() => {
-createWindow()
+  createWindow()
 
-app.on('activate', function () {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
 
 app.on('window-all-closed', function () {
-if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
-}
